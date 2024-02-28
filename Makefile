ea00cf46ba278df851c05828c20dfd116330f1c7
mckaygerhard <mckaygerhard@gmail.com>
2024-02-28 18:03:58 -0400

    support to specify arch in upstream docker shit!
    * pass arch to second stage
    * use clean ans push target

--- Makefile
+++ Makefile
@@ -4,15 +4,17 @@ DOCKER_NAME := venenux/venenuxdock-$(DEBIAN_VERSION)
 
 all: clean build test
 
+full: clean build test push
+
 build:
 	shellcheck -s ksh ./build-archived-debian-image.sh
-	./build-archived-debian-image.sh $(DEBIAN_VERSION) $(DOCKER_NAME)
+	./build-archived-debian-image.sh $(DEBIAN_VERSION) $(DOCKER_NAME) $(DEBIAN_ARCH)
 
 test: build
-	docker run $(DOCKER_NAME):$(DEBIAN_VERSION) -c 'echo `cat /etc/os-version.txt` ok'
+	docker run $(DOCKER_NAME):$(DEBIAN_ARCH) -c 'echo `cat /etc/os-version.txt` ok'
 
 push:
-	docker push $(DOCKER_NAME):$(DEBIAN_VERSION)
+	docker push $(DOCKER_NAME):$(DEBIAN_ARCH)
 
 clean:
-	docker image prune -a -f && docker rmi -f $(DOCKER_NAME):$(DEBIAN_VERSION)
+	docker image prune -a -f && docker rmi -f $(DOCKER_NAME):$(DEBIAN_ARCH)
--- build-archived-debian-image.sh
+++ build-archived-debian-image.sh
@@ -21,24 +21,27 @@ fi
 export DIST=${1:-jessie}
 if [ -n "$2" ]; then export DOCKER_NAME=${2}; else export DOCKER_NAME=venenux/venenuxdock-${DIST}; fi
 export DEBIAN_MIRROR=${DEBIAN_MIRROR:="http://archive.debian.org/debian/"}
-#export DEBIAN_ARCH:="$(dpkg-architecture -qDEB_BUILD_ARCH)"
+DEBIAN_ARCH="$(dpkg-architecture -qDEB_BUILD_ARCH)"
+if [ -n "$3" ]; then export DEBIAN_ARCH=${3}; else export DEBIAN_ARCH=${DEBIAN_ARCH}; fi
 
 # Check if rebuild is needed:
-LASTMOD=$(curl -sI "${DEBIAN_MIRROR}/dists/${DIST}/main/binary-amd64/Packages.gz" | \
+LASTMOD=$(curl -sI "${DEBIAN_MIRROR}/dists/${DIST}/main/binary-${DEBIAN_ARCH}/Packages.gz" | \
                  sed -n 's#^last-modified: *##pi' | \
                  sed -e 's#[^a-zA-Z0-9: -]##g')
 
-echo "Last modification of source: ${LASTMOD}"
+echo "Last modification of source: ${LASTMOD} of ${DOCKER_NAME}:${DEBIAN_ARCH}"
 
-docker pull "${DOCKER_NAME}:${DIST}" 2>/dev/null 1>&2 || true
+docker pull "${DOCKER_NAME}:${DEBIAN_ARCH}" 2>/dev/null 1>&2 || true
 CURRENTLAST="$(docker inspect --format='{{ index .Config.Labels "last_modified_src"}}' \
-                      "${DOCKER_NAME}:${DIST}" 2>/dev/null||echo 'non-existent')"
+                      "${DOCKER_NAME}:${DEBIAN_ARCH}" 2>/dev/null||echo 'non-existent')"
 
 # check if you will have bugs with bootstrap recent kernels respect glibc/dietlibc
 case $(uname -m) in
-  x86_64*)
-        echo "Your kernel must be boot with  vsyscall=emulate to work this docker builds (ENTER TO CONTINUE AND IGNORE)";
-        read -rt 50
+  x86_64)
+        if [ "${DEBIAN_ARCH}" == "amd64" ] ; then
+        echo "Your kernel must be boot with  vsyscall=emulate to work this docker builds, don try to run inside windoser stupid OS";
+        read -rt 90
+        fi
     ;;
   *)
     ;;
@@ -65,27 +68,27 @@ EOF
            --privileged \
            --cidfile=cif \
            debian-archived-builder \
-           "mkdir '/debian-${DIST}' \
-           && cdebootstrap --verbose --allow-unauthenticated --exclude=exim4\
-           'Debian/${DIST}' '/debian-${DIST}' \
+           "mkdir '/debian-${DIST}-${DEBIAN_ARCH}' \
+           && cdebootstrap --verbose --allow-unauthenticated --exclude=exim4 --arch=${DEBIAN_ARCH} \
+           'Debian/${DIST}' '/debian-${DIST}-${DEBIAN_ARCH}' \
            '${DEBIAN_MIRROR}'"
 
     STAGE1_ID="$(cat cif ; rm cif)"
     STAGE2_ID="$(docker commit "${STAGE1_ID}")"
 
     docker build \
-           -t "${DOCKER_NAME}:${DIST}" \
+           -t "${DOCKER_NAME}:${DEBIAN_ARCH}" \
            --label "last_modified_src=${LASTMOD}" \
            . \
            -f - <<EOF
 FROM ${STAGE2_ID} AS build
 FROM scratch
-COPY --from=build /debian-${DIST} /
+COPY --from=build /debian-${DIST}-${DEBIAN_ARCH} /
 ENV DEBIAN_FRONTEND noninteractive
 ENV LC_ALL C
 ENV LANGUAGE C
 LABEL maintainer="VenenuX"
-RUN /bin/echo "debian:${DIST}" > /etc/os-version.txt
+RUN /bin/echo "debian:${DIST}:${DEBIAN_ARCH}" > /etc/os-version.txt
 RUN /bin/echo -e "deb http://archive.debian.org/debian/ ${DIST} main contrib non-free\ndeb http://archive.debian.org/debian/ ${DIST}-backports main contrib non-free\ndeb http://archive.debian.org/debian-security/ ${DIST}/updates main contrib non-free" > /etc/apt/sources.list && apt-get update
 ENTRYPOINT [ "/bin/sh" ]
 EOF
